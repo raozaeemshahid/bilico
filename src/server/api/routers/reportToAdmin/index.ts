9402fc42416a57675d4a9d896f3d5d5836fb15e0
Rao Zaeem Shahid <raozaeem2018@gmail.com>
2023-03-15 22:34:14 +0500

    report page api added and post page added

--- src/server/api/routers/reportToAdmin/index.ts
+++ src/server/api/routers/reportToAdmin/index.ts
@@ -1,6 +1,8 @@
 import { createTRPCRouter } from "../../trpc";
 import { requestToUnban } from "./requestToUnban";
+import { reportPost } from "./reportPost";
 
 export const reportToAdmin = createTRPCRouter({
   requestToUnban,
+  reportPost,
 });
---
+++ src/server/api/routers/reportToAdmin/reportPost.ts
@@ -0,0 +1,27 @@
+import { z } from "zod";
+import { protectedProcedure } from "../../trpc";
+import PagesLinks from "../../../../lib/PagesLink";
+
+export const reportPost = protectedProcedure
+  .input(z.object({ postId: z.string().uuid() }))
+  .mutation(async ({ input, ctx }) => {
+    const previousReport = await ctx.prisma.reportToAdmin.findFirst({
+      where: {
+        postId: input.postId
+      },
+    });
+    if (previousReport) return { success: true };
+
+    await ctx.prisma.reportToAdmin.create({
+      data: {
+        link: PagesLinks.getPostLink(input.postId),
+        type: "POST",
+        ReportedPost: {
+          connect: {
+            id: input.postId,
+          },
+        },
+      },
+    });
+    return {success: true}
+  });
