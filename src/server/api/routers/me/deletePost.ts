32bb1cc08b4852755fb1e435ab9037ca35174f9f
Rao Zaeem Shahid <raozaeem2018@gmail.com>
2023-03-19 14:31:38 +0500

    Bug fixed

--- src/server/api/routers/me/deletePost.ts
+++ src/server/api/routers/me/deletePost.ts
@@ -4,17 +4,6 @@ import { z } from "zod";
 export const DeletePost = protectedProcedure
   .input(z.object({ postId: z.string().uuid() }))
   .mutation(async ({ ctx, input }) => {
-    await ctx.prisma.post.update({
-      where: { id: input.postId },
-      data: {
-        Comments: {
-          deleteMany: {},
-        },
-        Reactions: {
-          deleteMany: {},
-        },
-      },
-    });
     await ctx.prisma.post.delete({
       where: {
         id: input.postId,
